823068afd2a8a97210063848c940292323e4f5ee
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-01-30 03:12:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,5 +33,5 @@ end
 def using_concat(array)
   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
-  @my_favorite_things.concat(more_favs)
+  @all_my_favs.concat(@my_favorite_things).concat(more_favs)
 end
