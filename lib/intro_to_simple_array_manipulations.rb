0d5fdee6223e99eec4fb91f95ef61a36b59f01aa
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-01-30 01:33:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,11 @@
-def using_push(array, string)
+def using_push(array, String)
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
   next_color = "violet"
   colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(array, String)
+  boroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  new_neighborhood = "Staten Island"
+  boroughs_in_nyc.unshift(new_neighborhood)
 end
