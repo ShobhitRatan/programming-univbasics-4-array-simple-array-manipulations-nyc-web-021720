c8a590e24709033394b3e4a4fa51b766eb18dc55
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-01-30 02:56:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,3 +29,9 @@ def shift_with_args(array)
   @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
   @brands_removed = @ice_cream_brands.shift(2)
 end
+
+def using_concat(array, array)
+  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
+  @all_my_favs = @all_my_favs.concat(@my_favorite_things, more_favs)
+end
